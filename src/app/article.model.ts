2452d08b7258d2d6b0d80ffd25a3f7bcf574930d
victor tung <vtfsdev@gmail.com>
2020-12-04 10:38:03 -0600

    Completed Articles view

---
+++ src/app/article.model.ts
@@ -0,0 +1,11 @@
+export class Article {
+    _id: string;
+    title: string;
+    slug: string;
+    description: string;
+    keywords: string;
+    body: string;
+    published: Date;
+    created: Date;
+    modified: Date;
+}
